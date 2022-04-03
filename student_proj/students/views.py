3999bacd961c86e24ded0ef5992b25d06d127844
lllaserman <19931996am@gmail.com>
2022-04-03 11:19:19 +0300

    Add dynamics on student_list.html

--- student_proj/students/views.py
+++ student_proj/students/views.py
@@ -4,7 +4,25 @@ from django.http import HttpResponse
 #  Views for Students
 
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+        {'id' : 1,
+         'first_name' : 'Дмитро',
+         'last_name' : 'Подоба',
+         'ticket' : 235,
+         'image' : 'img/Podoba.jpg'},
+        {'id': 2,
+         'first_name': 'Максим',
+         'last_name': 'Кашпаров',
+         'ticket': 234,
+         'image': 'img/Kashparov.jpg'},
+        {'id': 3,
+         'first_name': 'Мария',
+         'last_name': 'Богак',
+         'ticket': 233,
+         'image': 'img/Bohak.jpg'},
+    )
+    return render(request, 'students/students_list.html',
+                  {'students' : students})
 
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
