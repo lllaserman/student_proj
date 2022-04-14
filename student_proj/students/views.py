417eb3a38aac10cd6f5b0dcb382f0f726d6d7816
lllaserman <19931996am@gmail.com>
2022-04-14 12:21:07 +0300

    Add base.html and correct another

--- student_proj/students/views.py
+++ student_proj/students/views.py
@@ -36,7 +36,8 @@ def students_delete(request, sid):
 # Views for Groups
 
 def groups_list(request):
-    return HttpResponse('<h1>Groups Listing</h1>')
+    return render(request, 'students/groups_list.html',
+                  {})
 
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
