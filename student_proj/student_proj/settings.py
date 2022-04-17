32fe6a6542ed652dabc73c1a01fbe78063c30a27
lllaserman <19931996am@gmail.com>
2022-04-17 10:40:18 +0300

    Add dynamics URL

--- student_proj/student_proj/settings.py
+++ student_proj/student_proj/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'students_proj.context_processors.students_proc',
             ],
         },
     },
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PORTAL_URL = 'http://localhost:8000'
